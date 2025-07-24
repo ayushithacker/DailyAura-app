ad4f7394e208f5a61f4c15bab6e2efb920c580a4
ayushithacker <ayushiathacker@gmail.com>
2025-07-24 18:55:00 +0530

    add logs for debugging

--- client/src/pages/OAuthSuccess.tsx
+++ client/src/pages/OAuthSuccess.tsx
@@ -6,26 +6,31 @@ const OAuthSuccess = () => {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
 
+  console.log("🌐 FULL URL:", window.location.href);
+  console.log("🔍 searchParams.get('token'):", searchParams.get("token"));
+
   useEffect(() => {
     const token = searchParams.get("token");
+
+    console.log("✅ Inside useEffect");
+    console.log("🛑 Token:", token);
 
     if (token) {
       localStorage.setItem("token", token);
       toast.success("Login successful via Google 🙌");
-      navigate("/"); 
+      navigate("/");
     } else {
       toast.error("OAuth login failed.");
       navigate("/login");
     }
-  }, [[searchParams, navigate]]);
+  }, [searchParams, navigate]);
 
-   return (
-  <div className="text-center mt-20 text-lg">
-    <p>Logging in via Google...</p>
-    <p>Token: {searchParams.get("token") || "No token found"}</p>
-  </div>
-);
-
+  return (
+    <div className="text-center mt-20 text-lg">
+      <p>Logging in via Google...</p>
+      <p>Token: {searchParams.get("token") || "No token found"}</p>
+    </div>
+  );
 };
 
 export default OAuthSuccess;
